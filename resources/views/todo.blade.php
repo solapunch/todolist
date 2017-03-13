af9bc135102b87c6b7425308bb19768e56b6eccd
solapunch <solapunch@gmail.com>
2017-03-13 11:40:00 +0700

    initial commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<h1>ToDo List</h1>
+	<ul>
+		<li>Taki</li>
+		<li>Mitsuha</li>
+		<li>Yotsuha</li>
+	</ul>
+	<form>
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="form-group">
+					<input type="text" class
+		<input type="text" name="todo_item" placeholder="item baru...">
+				</div>
+			</div>
+			<div class="col-lg-1">
+				<button type="submit" class="btn btn-success">Add</button>
+			</div>
+		</div>
+	</form>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
