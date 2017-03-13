996f2a4d3865f2dcc455c045fdd95a61cc053b53
solapunch <solapunch@gmail.com>
2017-03-13 12:47:11 +0700

    2nd
    Second try

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -3,15 +3,17 @@
 <head>
 	<title>ToDo List</title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body>
 	<h1>ToDo List</h1>
 	<ul>
-		<li>Taki</li>
-		<li>Mitsuha</li>
-		<li>Yotsuha</li>
+		@foreach($todo as $td)
+		<li>{{ $td->item }}</li>
+		@endforeach
 	</ul>
-	<form>
+	<form action="todo" method="POST">
+		{{ csrf_field() }}
 		<div class="row">
 			<div class="col-lg-3">
 				<div class="form-group">
